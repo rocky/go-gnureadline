86150b8dc2b0d764d9276e4b93eb32d7217b92c3
Rocky Bernstein <rocky@gnu.org>
2013-03-10 14:44:32 -0400

    Fill in more of the GNU readline interface

--- gnureadline-demo/main.go
+++ gnureadline-demo/main.go
@@ -1,9 +1,55 @@
 package main
-import ( . "gnureadline"; "fmt")
+import ( . "gnureadline"; "fmt"; "os")
+
+func print_edit_mode() {
+	if Rl_editing_mode() == 1 {
+		fmt.Println("Editing mode is emacs")
+	} else {
+		fmt.Println("Editing mode is vi")
+	}
+}
+
+func print_insert_mode() {
+	if Rl_insert_mode() == 1 {
+		fmt.Println("Insert mode on")
+	} else {
+		fmt.Println("Overwrite mode on")
+	}
+}
 func main() {
 	var line string
+	term := os.ExpandEnv("TERM")
+
+	fmt.Printf("Your readline library version is %s\n", 
+		Rl_readline_library_version())
+	fmt.Printf("Your readline version is %x\n", 
+		Rl_readline_version())
+	fmt.Printf("Your readline terminal is %s\n", 
+		Rl_readline_terminal_name())
+	fmt.Printf("Your readline terminal is %s\n", 
+		Rl_readline_name())
+	fmt.Printf("rl_prefer_env_winsize is %d\n", 
+		Rl_prefer_env_winsize())
+	print_edit_mode()
+	print_insert_mode()
+	line = Readline("Enter something without history: ")
 	for i:=1; line != "quit"; i++ {
-		line = Readline(fmt.Sprintf("Enter something %d: ", i))
+		line = Readline(fmt.Sprintf("Enter something %d: ", i), true)
+		switch line {
+		case "vi":
+			Rl_editing_mode_set(0)
+			print_edit_mode()
+		case "emacs":
+			Rl_editing_mode_set(1)
+			print_edit_mode()
+		case "insert":
+			Rl_insert_mode_set(1)
+			print_insert_mode()
+		case "overwrite":
+			Rl_insert_mode_set(0)
+			print_insert_mode()
+		}
 		fmt.Printf("You typed: %s\n", line)
 	}
+	Rl_reset_terminal(term)
 }
--- gnureadline.go
+++ gnureadline.go
@@ -10,9 +10,143 @@ package gnureadline
 import "C"
 import "unsafe"
 
-func Readline(prompt string) string {
+/*
+Calling seuence:
+    gnureadline.Readline(prompt, add_hist = false) string
+
+Readline will read a line from the terminal and return it, using prompt
+as a prompt.  If prompt the  empty  string,  no  prompt  is
+issued.  The  line  returned  has  the  final  newline
+removed, so only the text of the line remains.
+
+Readline offers editing capabilities while the user is entering the
+line.  By default, the line editing commands are similar to those of
+emacs.  A vi-style line editing interface is also available.
+
+*/
+func Readline(prompt string add_history ... bool) string {
 	c_prompt := C.CString(prompt)
 	defer C.free(unsafe.Pointer(c_prompt))
-	return C.GoString(C.readline(c_prompt))
+	c_line := C.readline(c_prompt)
+	defer C.free(unsafe.Pointer(c_line))
+	if len(add_history) > 0 && add_history[0] == true {
+		C.add_history(c_line)
+	}
+		
+	return C.GoString(c_line)
+}
+
+/*
+     Modify the terminal settings for Readline's use, so `readline()'
+     can read a single character at a time from the keyboard.  The
+     META_FLAG argument should be non-zero if Readline should read
+     eight-bit input.
+*/
+func Rl_prep_terminal(meta_flag int) {
+	C.rl_prep_terminal(C.int(meta_flag))
+}
+
+/*
+     Undo the effects of `rl_prep_terminal()', leaving the terminal in
+     the state in which it was before the most recent call to
+     `rl_prep_terminal()'.
+*/
+func Rl_deprep_terminal() {
+	C.rl_deprep_terminal()
+}
+
+/*
+     Ring the terminal bell, obeying the setting of `bell-style'.
+*/
+func Rl_ding () int {
+	return int(C.rl_ding())
+}
+
+/*
+     Reinitialize Readline's idea of the terminal settings using
+     TERMINAL_NAME as the terminal type (e.g., `vt100').  If
+     TERMINAL_NAME is the empty string, the value of the `TERM' environment
+     variable is used.
+*/
+func Rl_reset_terminal(terminal_name string) int {
+	if len(terminal_name) == 0 {
+		return int(C.rl_reset_terminal(nil))
+	}
+	c_terminal_name := C.CString(terminal_name)
+	defer C.free(unsafe.Pointer(c_terminal_name))
+	return int(C.rl_reset_terminal(c_terminal_name))
+}
+
+
+// I miss Ruby's attr_reader
+
+/* The version number of this revision of the library. e.g. "6.2" */
+func Rl_readline_library_version() string {
+	return C.GoString(C.rl_library_version)
+}
+
+/*
+     The terminal type, used for initialization.  If not set by the
+     application, Readline sets this to the value of the `TERM'
+     environment variable the first time it is called.
+*/
+func Rl_readline_terminal_name() string {
+	return C.GoString(C.rl_terminal_name)
+}
+
+/*
+     This variable is set to a unique name by each application using
+     Readline.  The value allows conditional parsing of the inputrc file
+*/
+func Rl_readline_name() string {
+	return C.GoString(C.rl_readline_name)
+}
+
+/*
+     An integer encoding the current version of the library.  The
+     encoding is of the form 0xMMMM, where MM is the two-digit major
+     version number, and MM is the two-digit minor version number.  For
+     example, for Readline-4.2, `rl_readline_version' would have the
+     value 0x0402.
+*/
+func Rl_readline_version() int {
+	return int(C.rl_readline_version)
+}
+
+/*
+     If non-zero, Readline gives values found in the `LINES' and
+     `COLUMNS' environment variables greater precedence than values
+     fetched from the kernel when computing the screen dimensions.
+*/
+func Rl_prefer_env_winsize() int {
+	return int(C.rl_prefer_env_winsize)
+}
+
+/* Says which editing mode readline is currently using.  1 means emacs mode;
+   0 means vi mode. */
+func Rl_editing_mode() int {
+	return int(C.rl_editing_mode)
+}
+
+/* Insert or overwrite mode for emacs mode.  1 means insert mode; 0 means
+   overwrite mode.  Reset to insert mode on each input line. */
+func Rl_insert_mode() int {
+	return int(C.rl_insert_mode)
+}
+
+// I miss Ruby's attr_accessor
+
+/* Set editing mode readline is currently using.  1 means emacs mode;
+   0 means vi mode. */
+func Rl_editing_mode_set(new_value int) int {
+	C.rl_editing_mode = C.int(new_value)
+	return int(C.rl_editing_mode)
+}
+
+/* Set insert or overwrite mode for emacs mode.  1 means insert mode; 0 means
+   overwrite mode.  Reset to insert mode on each input line. */
+func Rl_insert_mode_set(new_value int) int {
+	C.rl_insert_mode = C.int(new_value)
+	return int(C.rl_insert_mode)
 }
 
