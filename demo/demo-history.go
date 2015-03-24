9244a1924925c65d326ad8ce31b512b12c881c41
rocky <rb@dustyfeet.com>
2015-03-24 12:46:25 -0400

    Change demo import and maybe this willl fix travis. try #5

--- demo/demo-history.go
+++ demo/demo-history.go
@@ -1,17 +1,20 @@
 package main
-import ( . "gnureadline"; "fmt")
+import (
+	"fmt"
+	"github.com/rocky/go-gnureadline"
+)
 
 func main() {
-	fmt.Printf("history_expansion_char is %c\n", 
-		   HistoryExpansionChar())
-	fmt.Printf("history_subst_char is %c\n", 
-		   HistorySubstChar())
-	fmt.Printf("history_comment_char is %c\n", 
-		   HistoryCommentChar())
-	fmt.Printf("History length %d\n",  HistoryLength())
-	fmt.Printf("History max entries %d\n",  HistoryMaxEntries())
-	fmt.Println("History is stifled", HistoryIsStifled())
-	StifleHistory(3)
-	fmt.Println("History is stifled", HistoryIsStifled())
+	fmt.Printf("history_expansion_char is %c\n",
+		   gnureadline.HistoryExpansionChar())
+	fmt.Printf("history_subst_char is %c\n",
+		   gnureadline.HistorySubstChar())
+	fmt.Printf("history_comment_char is %c\n",
+		   gnureadline.HistoryCommentChar())
+	fmt.Printf("History length %d\n",  gnureadline.HistoryLength())
+	fmt.Printf("History max entries %d\n",  gnureadline.HistoryMaxEntries())
+	fmt.Println("History is stifled", gnureadline.HistoryIsStifled())
+	gnureadline.StifleHistory(3)
+	fmt.Println("History is stifled", gnureadline.HistoryIsStifled())
 
 }
